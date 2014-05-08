81dd2c3504dbc47ab0257c435bb8fa7d2d5ef883
Umesh Unnikrishnan <bitbucket@umeshunni.com>
2014-05-07 21:24:01 -0700

    new file:   7-segment.py

---
+++ rpi/7-segment.py
@@ -0,0 +1,41 @@
+### Uses GPIO to display numbers on a ELS-321HDB 7-segment display connected via the GPIO pins to a Raspberry Pi 
+
+import RPi.GPIO as GPIO
+import time
+
+
+
+segments = [('A', 'B', 'C','D','E','F' ),
+            ('B','C'), #1
+            ('A','B','D','E','G'), #2
+            ('A','B','C','D','G'), #3
+            ('B','C', 'F','G'), #4
+            ('A','C', 'D','F','G'), #5
+            ('A','C', 'D','E','F','G'), #6
+            ('A','B','C'), #7
+            ('A','B','C', 'D','E','F','G'), #8
+            ('A','B','C', 'D','F','G')] #9
+
+pins = {'A': 17, 'B':27, 'C':22, 'D':25, 'E':24, 'F':18, 'G':23}
+
+#GPIO functions
+
+def init_gpio:
+  GPIO.setmode(GPIO.BCM)
+
+def display_nothing:
+  GPIO.cleanup()
+
+def display_number(num):
+  for pin in segments[num]:
+    print pins[num]
+    GPIO.setup(pins[num], GPIO.OUT)
+    GPIO.output(pins[num], GPIO.HIGH)
+  sleep(10)
+  print "reset"
+  display_nothing()
+
+for i in range (9):
+  display_number(i)
+
+
