cec709da4ad9ede6c70ad588dfaaede91c54a9a7
UmeshU <github@umeshunni.com>
2014-06-03 23:45:29 -0700

    Some code clean up and modularity

---
+++ board.py
@@ -0,0 +1,104 @@
+import random
+
+
+class Cell(object):
+    def __init__(self, is_mine, is_visible=False, is_flagged=False):
+        self.is_mine = is_mine
+        self.is_visible = is_visible
+        self.is_flagged = is_flagged
+
+    def show(self):
+        self.is_visible = True
+
+    def flag(self):
+        self.is_flagged = not self.is_flagged
+
+    def place_mine(self):
+        self.is_mine = True
+
+
+class Board(tuple):
+    def __init__(self, tup):
+        super().__init__()
+        self.is_playing = True
+
+    def mine_repr(self,row_id, col_id):
+        cell = self[row_id][col_id]
+        if cell.is_visible:
+            if cell.is_mine:
+                return "M"
+            else:
+                surr = self.count_surrounding(row_id, col_id)
+                return str(surr) if surr else " "
+        elif cell.is_flagged:
+            return "F"
+        else:
+            return "."  #u"\uff18"
+
+    def __str__(self):
+        board_string = ("Mines: " + str(self.remaining_mines) + "\n  " +
+                        "".join([str(i) for i in range(len(self[0]))]))
+        for (row_id, row) in enumerate(self):
+            board_string += ("\n" + str(row_id) + " " + 
+                             "".join(self.mine_repr(row_id, col_id) for (col_id, _) in enumerate(row)) +
+                             " " + str(row_id))
+        board_string += "\n  " + "".join([str(i) for i in range(len(self[0]))])
+        return board_string
+
+    def show(self, row_id, col_id):
+        cell = self[row_id][col_id]
+        if not cell.is_visible:
+            cell.show()
+
+            if (cell.is_mine and not
+                cell.is_flagged):
+                self.is_playing = False
+            elif self.count_surrounding(row_id, col_id) == 0:
+                for (surr_row, surr_col) in self.get_neighbours(row_id, col_id):
+                    if self.is_in_range(surr_row, surr_col):
+                        self.show(surr_row, surr_col) 
+
+    def showall(self):
+        for row in self:
+            for cell in row:
+                cell.show()
+        print(self)               
+        
+    def flag(self, row_id, col_id):
+        cell = self[row_id][col_id]
+        if not cell.is_visible:
+            cell.flag()
+        else:
+            print("Cannot add flag, cell already visible.")
+
+    def place_mine(self, row_id, col_id):
+        self[row_id][col_id].place_mine()
+
+    def count_surrounding(self, row_id, col_id):
+        return sum(1 for (surr_row, surr_col) in self.get_neighbours(row_id, col_id)
+                        if (self.is_in_range(surr_row, surr_col) and
+                            self[surr_row][surr_col].is_mine))
+
+    def get_neighbours(self, row_id, col_id):
+        SURROUNDING = ((-1, -1), (-1,  0), (-1,  1),
+                       (0 , -1),           (0 ,  1),
+                       (1 , -1), (1 ,  0), (1 ,  1))
+        return ((row_id + surr_row, col_id + surr_col) for (surr_row, surr_col) in SURROUNDING)
+
+    def is_in_range(self, row_id, col_id):
+        return 0 <= row_id < len(self) and 0 <= col_id < len(self[0])
+
+    @property
+    def remaining_mines(self):
+        remaining = 0
+        for row in self:
+            for cell in row:
+                if cell.is_mine:
+                    remaining += 1
+                if cell.is_flagged:
+                    remaining -= 1
+        return remaining
+
+    @property
+    def is_solved(self):
+        return all((cell.is_visible or cell.is_flagged) for row in self for cell in row)
