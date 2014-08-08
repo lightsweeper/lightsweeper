99b4c480cee6a74271d924ddc7a8a4621b1826d0
jaywardle <wardlesathome@earthlink.net>
2014-08-07 20:27:14 -0700

    sync up with git change
    setSegments() support for custom tile display
    lots of test support
    clean out stale data when expecting read data

--- LSRealTile.py
+++ LSRealTile.py
@@ -16,6 +16,18 @@ import time
 
 # these constants copied from LSTileAPI.h
 
+# one byte commands for special test modes
+# some of these can be used to visually locate the addressed tile
+NOP_MODE        = 0     # this command changes nothing
+SENSOR_TEST     = 1     # single digit ADC voltage, color changes at threshold
+SENSOR_STATS    = 2 
+SEGMENT_TEST    = 3     # walks through all colors of all digits
+FASTEST_TEST    = 4     # walks through all colors of all digits fast, looks white
+ROLLING_FADE_TEST = 5   # fades in and out from inside to out
+ROLLING_FADE_TEST2 = 6  # fades in and out from inside to out
+SHOW_ADDRESS    = 7     # display serial address for floor setup
+STOP_MODE       = 0xF   # tile stops updating display
+
 LS_LATCH = 0x10       # refresh display from queue - usually address 0
 LS_CLEAR = LS_LATCH+1 # blanks the tile
 LS_RESET = LS_LATCH+2 # reboot
@@ -55,8 +67,10 @@ TILE_VERSION = (ADC_NOW + 9) # format TBD - prefer one byte
 
 # EEPROM read is command and one byte of address
 EEPROM_READ  =   0x60
-# EEPROM write is command, one address byte, and one data byte
+# EEPROM write is two byte command, one address byte, one data byte, and checksum
 EEPROM_WRITE =   (EEPROM_READ+1)
+EEPROM_WRITE2 =  (0x53)
+
 # Defined EEPROM addresses:
 EE_ADDR   =  0 # 0 - tile address in top five bytes
 EE_CONFIG =  1 # 1 - tile configuration
@@ -78,6 +92,64 @@ RETURN_ERRORS =  (ERROR_CMD+1) # returns the last MAX_ERRORS errors in queue
         # Most recent errors are returned first
         # Clears error queue and STATUS_ERR_MASK
 CLEAR_ERRORS  = (ERROR_CMD+2)  # not really needed, but nearly free
+
+# Segment display commands from 0x80 to 0xBF
+SEGMENT_CMD =   0x80
+SEGMENT_CMD_END = (SEGMENT_CMD+0x3F)
+# Depending on the command, up to 4 byte fields will follow (R,G,B and transition)
+# Three bits in command declare that R, G, and/or B segment fields will follow
+# Two bits define the update condition
+# One bit declares that the transition field will follow
+#
+# One segment byte field will be provided for each of the RGB color bits declared
+# Three segment fields allow for arbitrary colors for each segment
+# Segment fields are defined in the -abcdefg order, to match LedControl library
+SEGMENT_FIELD_MASK  = 0x38
+SEGMENT_FIELD_RED   = 0x20
+SEGMENT_FIELD_GREEN = 0x10
+SEGMENT_FIELD_BLUE  = 0x08
+# Segment fields that are not given clear the associated target color segments
+# unless the LSB is set in one of the provided segment fields
+SEGMENT_KEEP_MASK  = 0x80 # if MSB set, do not clear any segment data
+
+# The update condition bits define when these segments are applied to the display
+# There are three update events: immediate, LATCH commands or a sensor detection
+# Only four combinations make sense since immediate trumps the other two
+# 00 - segment information is immediately applied to the active display
+# 01 - segment information is applied after an LATCH command
+# 10 - segment information is applied when the sensor detects weight
+# 11 - segment information is applied when the sensor detects weight or LATCH
+CONDX_MASK       = 0x06
+CONDX_IMMED      = 0x00
+CONDX_LATCH      = 0x02
+CONDX_TRIG       = 0x04
+CONDX_LATCH_TRIG = 0x06
+#
+# The transition bit means a final byte will be used as the transition effect
+# These transitions are TBD.
+TRANSITION_FIELD_MASK = 0x01
+#
+# These examples do not include the tile addressing byte -
+#
+# Set the segments to transition to a blue 4 on the next LATCH:
+# B Segments at LATCH  B=bcfg
+# 10 001 01 0          00110011
+# 0x8A                 0x33
+#
+# Set the segments to a red white and blue 8 at a sensor trigger:
+# RGB Segments at trigger  R=acdfg   G=adg     B=abdeg
+# 10 111 10 0              01011011  01001001  01101101
+# 0xBC                     0x5B      0x49      0x6D
+#
+# Immediately set a yellow 6 with transition effect #7:
+# Immediate RGB Segments   R=abcdeg  G=abcdeg Transition #7
+# 10 110 00 1              01111101  01111101 00000111 (TBD)
+# 0xB1                     0x7D      0x7D     0x07 (TBD)
+#
+# Clear the active display immediately - alternative way to using LS_CLEAR:
+# Immediately clear RGB by giving no segment field data
+# 10 000 00 0
+# 0x80
 
 
 
@@ -101,7 +173,6 @@ class LSRealTile(LSTileAPI):
     # set immediately or queue this color in addressed tiles
     def setColor(self, color):
         cmd = SET_COLOR
-        #print("writing ", cmd, color)
         self.__tileWrite([cmd, color])
 
     def setShape(self, shape):
@@ -115,6 +186,54 @@ class LSRealTile(LSTileAPI):
     def setTransition(self, transition):
         cmd = SET_TRANSITION
         self.__tileWrite([cmd, self.shape])
+
+    # rgb is a three element list of numbers from 0 (no segments of this color) to 127 (all 7 segments lit)
+    # If any element is None, the colors of unspecified fields is preserved
+    # Segment fields are defined in the -abcdefg order, to match LedControl library
+    def setSegments(self, rgb, conditionLatch = False, conditionTrig = False ):
+        cmd = SEGMENT_CMD
+        args = []
+        clear = True # default to clearing ungiven colors
+        # determine if clear non stated colors or keeps them
+        if rgb[0] == None:
+            clear = False
+        elif rgb[1] == None:
+            clear = False
+        elif rgb[2] == None:
+            clear = False
+
+        # One segment byte field will be provided for each of the RGB color bits declared
+        # Three segment fields allow for arbitrary colors for each segment
+        if rgb[0] != None and rgb[0] >= 1:
+            field = rgb[0]
+            if not(clear):
+                field |= SEGMENT_KEEP_MASK
+            cmd += SEGMENT_FIELD_RED
+            args.append(field)
+        if rgb[1] != None and rgb[1] >= 1:
+            field = rgb[1]
+            if not(clear):
+                field |= SEGMENT_KEEP_MASK
+            cmd += SEGMENT_FIELD_GREEN
+            args.append(field)
+        if rgb[2] != None and rgb[2] >= 1:
+            field = rgb[2]
+            if not(clear):
+                field |= SEGMENT_KEEP_MASK
+            cmd += SEGMENT_FIELD_BLUE
+            args.append(field)
+
+        # TODO - not tested in tile, probably broken
+        if conditionLatch:
+            cmd += CONDX_LATCH
+        if conditionTrig:
+            cmd += CONDX_TRIG
+
+        # TODO - effects transitions
+
+        args.insert(0, cmd) # couldn't insert cmd until all fields are added
+        self.__tileWrite(args)
+
 
     def set(self,color=0, shape=0, transition=0):
         raise NotImplementedError()
@@ -160,8 +279,12 @@ class LSRealTile(LSTileAPI):
     
     # eeAddr and datum from 0 to 255
     def eepromWrite(self,eeAddr,datum):
-        cmd = EEPROM_WRITE
-        self.__tileWrite([cmd, eeAddr, datum])
+        # EEPROM write is two byte command, one address byte, one data byte, and checksum
+        sum = EEPROM_WRITE + EEPROM_WRITE2 + eeAddr + datum
+        chk = (65536 - sum) % 256;
+        #chk = chk + 1 # TEST REMOVEME - this breaks checksum
+        print("eepromWrite computed sum = %d, checksum = %d" % (sum, chk))
+        self.__tileWrite([EEPROM_WRITE, EEPROM_WRITE2, eeAddr, datum, chk])
 
     # eeAddr from 0 to 255
     def eepromRead(self,eeAddr):
@@ -187,7 +310,7 @@ class LSRealTile(LSTileAPI):
 
     # send mode command that displays stuff
     def locate(self):
-        cmd = 7 # the SHOW_ADDRESS command
+        cmd = SHOW_ADDRESS
         self.__tileWrite([cmd])
 
     def sensorTest(self):
@@ -195,7 +318,7 @@ class LSRealTile(LSTileAPI):
         self.__tileWrite([cmd])
 
     def demo (self, seconds):
-        cmd = 3
+        cmd = SEGMENT_TEST
         self.__tileWrite([cmd])
 
     def setAnimation(self):
@@ -235,6 +358,26 @@ class LSRealTile(LSTileAPI):
         time.sleep(1.0)
         val = self.__tileRead()
 
+    # resynchronize communications
+    # this uses the global address, so it needs to be done only once per interface port
+    def syncComm(self):
+        # TODO - __tileWrite could handle global address, simpler to just copy code
+        if self.mySerial == None:
+            return
+
+        # sync command is two adjacent NOP_MODE commands
+        args = [0, NOP_MODE]  # use global address
+        count = self.mySerial.write(args)
+        count = self.mySerial.write(args)
+        if self.Debug:
+            print("sync command wrote two NOP_MODE commands")
+
+        # read to flush tile debug output
+        thisRead = self.mySerial.read(8)
+        if len(thisRead) > 0:
+            # Debug or not, if tile sends something, we want to see it
+            print ("Debug response: " + ' '.join(format(x, '#02x') for x in thisRead))
+
     def setDebug(self, debugFlag):
         cmd = LS_DEBUG
         self.Debug = debugFlag
@@ -267,6 +410,15 @@ class LSRealTile(LSTileAPI):
     def __tileWrite(self, args, expectResponse=False):
         if self.mySerial == None:
             return
+
+        # flush stale read data if response is expected
+        if (expectResponse):
+            thisRead = self.mySerial.read(8)
+            if len(thisRead) > 0:
+                # debug or not, if tile sends something, we want to see it
+                if True or self.Debug:
+                    print ("Stale response: " + ' '.join(format(x, '#02x') for x in thisRead))
+
         # insert address byte plus optional arg count
         addr = self.address + len(args) - 1  # command is not counted
         args.insert(0, addr)
@@ -281,7 +433,7 @@ class LSRealTile(LSTileAPI):
             if len(thisRead) > 0:
                 #if self.Debug:
                 # debug or not, if tile sends something, we want to see it
-                if False or self.Debug:
+                if True or self.Debug:
                     print ("Debug response: " + ' '.join(format(x, '#02x') for x in thisRead))
 
     # read from the tile
@@ -344,11 +496,6 @@ def serial_ports():
 def testSleep(secs=0.3):
     time.sleep(secs)
 
-# send sync sequence
-def serialSync(mySerial):
-    #print("sync")  # probably should not print since it slows down timing
-    mySerial.write([0, 0, 0, 0])
-    
 # full suite of command tests
 def fullSuite(myTile):
         print("\nTesting setDebug - off")
@@ -357,6 +504,11 @@ def fullSuite(myTile):
 
         print("\nTesting setDebug - on")
         myTile.setDebug(1)
+        testSleep()
+
+        print("\nInitial errorRead to clear errors")
+        testSleep()
+        myTile.errorRead()
         testSleep()
 
         print("\nTesting setColor - white")
@@ -368,8 +520,8 @@ def fullSuite(myTile):
         testSleep()
 
 
-        print("\nTesting setColor - red")
-        myTile.setColor(1)
+        print("\nTesting setColor - white")
+        myTile.setColor(7)
         testSleep()
 
         print("\nTesting setShape - 7")
@@ -469,25 +621,181 @@ def fullSuite(myTile):
 def commTest(myTile, mySerial):
         print("\nTesting setShape in many loops to look for timing bug")
         myTile.setColor(7)
+        tmpDebug = myTile.Debug
+        myTile.setDebug(0)
         for yuck in range(100):
+            myTile.setColor(7)
             print("\nTesting setShape - faster, with Debug off")
-            fastDelay = 0.01
+            fastDelay = 0.005 # 0.01
             fastTime = 3.0
-            tmpDebug = myTile.Debug
             start = time.time()
-            myTile.setDebug(0)
             for someLoops in range(0,100):  # loop a bunch to let this last long enough
                 for i in range(0,256):
                     if i%64 == 50:
-                        serialSync(mySerial)
+                    #if i%8 == 4:
+                        #myTile.syncComm()
+                        pass
                     myTile.setShape(i%256)
                     testSleep(fastDelay)  # may cause another OS swap of 15.6 ms
                 if (time.time() - start) > fastTime:
                     break;
 
-            print("Restoring prior Debug")
-            myTile.setDebug(tmpDebug)
-            
+        print("Restoring prior Debug")
+        myTile.setDebug(tmpDebug)
+
+def eepromRead(myTile, firstAddr, len):
+        for addr in range(firstAddr, firstAddr+len):
+            thisRead = myTile.eepromRead(addr)
+            #if len(thisRead) > 0:
+            if True:
+                print ("EEPROM address " + repr(addr) + " = "  + ' '.join(format(x, '#02x') for x in thisRead))
+            else:
+                print("No response for EEPROM read at address " + repr(addr))
+
+def eepromTasks(myTile):
+        # read first EEPROM addresses
+        myTile.setDebug(0)
+        print("Looping on EEPROM tests - Ctrl-C to exit")
+        for loop in range(1000):
+            eepromRead(myTile, 0, 8)
+            for action in range(8):
+                addr = ""
+                addr = input("Enter address of EEPROM to write: ")
+                if addr == "":
+                    #print ("OK - done for now")
+                    break #return
+                    pass
+                else:
+                    addr = int(addr)
+                    writeStr = input("Enter value to write: ")
+                    writeVal = int(writeStr)
+                    print("OK - write " + repr(writeVal) + " to address " + repr(addr))
+                    myTile.eepromWrite(addr, writeVal)
+                    print("Check errorRead:")
+                    myTile.errorRead()
+
+
+def addressWalk(myTile, mySerial):
+    for address in range(1,32):
+        myTile.assignAddress(address * 8)
+        result = myTile.getAddress()
+        myTile.setDebug(0)  # minimize visual clutter
+        #print("Tile address = " + result)
+        strRes = ""
+        #strRes = ''.join( [ "0x%02X " %  x for x in result ] ).strip()
+        strRes = repr(result)
+        #for i in result:
+        #    strRes  += "0x%s " % (i.encode('hex'))
+        print("\nReading EEPROM 0 at tile address = " + strRes)
+        testSleep()
+        #eepromRead(myTile, 0, 1)
+        addr = 0
+        thisRead = myTile.eepromRead(addr)
+        if len(thisRead) > 0:
+            print ("Address " + repr(addr) + " = "  + ' '.join(format(x, '#02x') for x in thisRead))
+        else:
+            #print("No response for EEPROM read at address " + repr(addr))
+            pass
+
+def setSegmentsTest(myTile):
+        myTile.setDebug(1)
+        print("\nInitial errorRead to clear errors")
+        testSleep()
+        myTile.errorRead()
+        testSleep()
+
+        print("\nall segments red")
+        rgb = [127,0,0]
+        myTile.setSegments(rgb)
+        testSleep(2)
+
+        print("\nall segments green")
+        rgb = [0,127,0]
+        myTile.setSegments(rgb)
+        testSleep(2)
+
+        print("\nall segments blue")
+        rgb = [0,0,127]
+        myTile.setSegments(rgb)
+        testSleep(2)
+
+        for loop in range(1):
+            seg = 1
+            for i in range(7):
+                print("\nmostly green with red " + repr(seg))
+                rgb = [seg, 127-seg, 0]
+                myTile.setSegments(rgb)
+                seg = seg * 2
+                testSleep()
+
+            seg = 1
+            for i in range(7):
+                print("\nmostly blue with green " + repr(seg))
+                rgb = [0, seg, 127-seg]
+                myTile.setSegments(rgb)
+                seg = seg * 2
+                testSleep()
+
+            seg = 1
+            for i in range(7):
+                print("\nmostly red with blue " + repr(seg))
+                rgb = [127-seg, 0, seg]
+                myTile.setSegments(rgb)
+                seg = seg * 2
+                testSleep()
+
+        print("\nTesting synchronized setSegments calls with latch")
+        for loop in range(1):
+            seg = 1
+            for i in range(7):
+                rgb = [seg, seg, seg] # queue segment
+                myTile.setSegments(rgb, conditionLatch = True)
+                print("segments downloaded, display updates on following latch command...")
+                testSleep(2)
+                print("latch !")
+                myTile.latch()
+                seg = seg * 2
+                print("")
+                testSleep(2)
+                
+        print("\nTesting multiple setSegments calls with latch")
+        for loop in range(3):
+            seg = 1
+            for i in range(7):
+                print("\nThree writes plus latch for white seg=" + repr(seg))
+                rgb = [seg, 0, 0] # queue red segment
+                myTile.setSegments(rgb, conditionLatch = True)
+                rgb = [None, seg, 0] # queue green segment, preserve other colors
+                myTile.setSegments(rgb, conditionLatch = True)
+                rgb = [0, None, seg] # queue blue segment, preserve other colors
+                myTile.setSegments(rgb, conditionLatch = True)
+                myTile.latch()
+                seg = seg * 2
+                testSleep()
+                
+        print("\ncolor chase - segments from a to f, no g. Debug off")
+        print("Ctrl-C to exit")
+        myTile.setDebug(0)
+        for loop in range(1000000):
+            seg = 2
+            #if (loop % 1000) == 0:
+            #    print("loop " + repr(loop))
+            if (loop % 30) == 0:
+                print("Checking errors, loop=" + repr(loop))
+                myTile.errorRead()
+            for i in range(6):  # six segment chase
+                red = seg # 2 to 64 for segment f to a
+                green = red * 2 # green is one segment CCW from red
+                if green == 128:
+                    green = 2
+                blue = green * 2 # blue is one segment CCW from green
+                if blue == 128:
+                    blue = 2
+                rgb = [red, green, blue]
+                myTile.setSegments(rgb)
+                seg = seg * 2
+                testSleep(0.08) # chase blurs when faster than this
+   
 def sensorTest(myTile):
        print("\nSetting sensor test mode")
        myTile.sensorTest()
@@ -565,6 +873,9 @@ def main():
         print("    2 - sensor test")
         print("    3 - sensorStatus test")
         print("    4 - run single digit mode")
+        print("    5 - EEPROM tasks")
+        print("    6 - walk the address space")
+        print("    7 - test setSegments API")
         testChoice = int(input("What test do you want to run? "))
         if testChoice == 0:
             fullSuite(myTile)
@@ -577,6 +888,12 @@ def main():
             sensorStatusTest(myTile)
         elif testChoice == 4:
             singleModeTest(theSerial)
+        elif testChoice == 5:
+            eepromTasks(myTile)
+        elif testChoice == 6:
+            addressWalk(myTile, theSerial)
+        elif testChoice == 7:
+            setSegmentsTest(myTile)
         else:
             print("You did not enter a valid test choice")
         
