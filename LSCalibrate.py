b6bd752d630cc9b0ae9f292e5de93bc0d191fb54
Russ Tarleton <russ.tarleton@gmail.com>
2014-08-16 14:57:52 -0700

    Added new LSCalibration file - wireframe comments only. This can potentially eventually be merged into LSRealFloor.

---
+++ LSCalibrate.py
@@ -0,0 +1,11 @@
+# Run this to calibrate a single tile's stepped-on sensor feedback
+
+# Launch LSCalibrate
+# Enter Row, Col index
+# Read Stepped-On Sensor Reading
+# Read Stepped-Off Sensor Reading
+# Read previous EEPROM
+# Previous Readings were: [Low/High], New Readings are: [Low/High] Do you wish to overwrite? [Y/N]
+# Write to EEPROM
+
+# Consider wrapping this into LSRealFloor main
