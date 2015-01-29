530cdeee81d18bf17e3e6f90d1c5828585cfa758
kryptographik <kryptographik@gmail.com>
2015-01-28 19:21:58 -0800

    First (lame) attempt at interactive configuration in LSFloorConfigure.py

--- LSFloorConfigure.py
+++ LSFloorConfigure.py
@@ -5,6 +5,7 @@ import os
 import serial
 import pickle
 from LSRealTile import lsOpen
+from LSRealTile import LSRealTile
 
 # prints the list of 4-tuples
 def printConfig(config):
@@ -20,36 +21,72 @@ def main():
 
     # serial ports are COM<N> on windows, /dev/xyzzy on Unixlike systems
     availPorts = list(tilepile.lsMatrix)
-    print(repr(availPorts))
+
     print("Available serial ports:" + str(availPorts))
-    defaultPort = "COM8"
+    
+    totaltiles = 0
+    for port in tilepile.lsMatrix:
+        totaltiles += len(tilepile.lsMatrix[port])
+
     if len(availPorts) > 0:  # default to the first port in the list
         defaultPort = availPorts[0]
+        
+    def pickgeom(numTiles, rows):
+        if rows > numTiles:
+            print("There are only " + repr(numTiles) + " tiles!")
+            return False
+        else:
+            return True
         
     pickleName = input("\nIf you have an existing configuration to edit, enter the name here: ")
     if "" == pickleName:
         print("\nStarting a new configuration from scratch.")
-        rows = input("\nEnter the number of rows in your floor:")
-        rows = int(rows) # we need a number
-        cols = input("Enter the number of columns in your floor:")
-        cols = int(cols) # we need a number
+        rows = int(input("\nHow many rows do you want?: "))
+        while pickgeom(totaltiles, rows) is False:
+            rows = int(input("\nHow many rows do you want?: "))
+        cols = int(totaltiles/rows)
+            
+        
 
         print("OK, you have a floor with " + repr(rows) + " by " + repr(cols)  + " columns")
 
         config = []
-
-        for row in range(rows):
-            for col in range(cols):
-                print("\nRow=" + repr(row) + " column=" + repr(col))
-                port = input("Enter the port name (just enter to use " + defaultPort + "): ")
-                if port == "":
-                    port = defaultPort
-                    #print("Using port " + port)
-                address = input("Enter the tile address: ")
-                address = int(address)
-                thisTile = (row, col, port, address)
+        
+        print("Blanking all tiles.")
+        for port in tilepile.lsMatrix:
+            myTile = LSRealTile(tilepile.lsSerial(port))
+            myTile.assignAddress(0)
+        #   myTile.blank()     # Not implemented in LSRealTile
+            myTile.setColor(0)  # A TEMPORARY hack
+            
+        for port in tilepile.lsMatrix:
+            for addr in tilepile.lsMatrix[port]:
+                print("Port is: " + repr(port) + " Address is: " + repr(addr))
+                myTile = LSRealTile(tilepile.lsSerial(port))
+                myTile.assignAddress(addr)
+                myTile.demo(1)
+                row=int(input("Which row?: "))
+                row = row-1
+                col=int(input("Which col?: "))
+                col = col-1
+                thisTile = (row, col, port, addr)
                 print("Added this tile: " + str(thisTile))
                 config.append(thisTile)
+                myTile.setColor(0)
+ 
+ # Old code:               
+#        for row in range(rows):
+#            for col in range(cols):
+#                print("\nRow=" + repr(row) + " column=" + repr(col))
+#                port = input("Enter the port name (just enter to use " + defaultPort + "): ")
+#                if port == "":
+#                    port = defaultPort
+#                    #print("Using port " + port)
+#                address = input("Enter the tile address: ")
+#                address = int(address)
+#                thisTile = (row, col, port, address)
+#                print("Added this tile: " + str(thisTile))
+#                config.append(thisTile)
 
         print("\nThis is the configuration: ")
         #print(repr(config))
