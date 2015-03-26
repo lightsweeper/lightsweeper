1b749f8abfc21ced8cf6785d929ea891c7275ac6
kryptographik <kryptographik@z3n.com>
2015-03-25 19:14:48 -0700

    Minor tweaks

--- lsexplosion.py
+++ lsexplosion.py
@@ -191,7 +191,7 @@ def main():
     except Exception as e:
         useRealFloor = False
 
-    d = lsdisplay.LSDisplay(realFloor = False, simulatedFloor = True, initScreen=False)
+    d = lsdisplay.LSDisplay(realFloor = True, simulatedFloor = True, initScreen=False)
     rows = d.rows
     cols = d.cols
 
